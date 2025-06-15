395984dfe8296d224ed9173dbabeb67785b19a83
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-06-15 20:33:12 +0530

    responsiveness

--- src/container/main-section/mainSection.tsx
+++ src/container/main-section/mainSection.tsx
@@ -5,8 +5,8 @@ import SocialMedia from "@/container/social-media/socialMedia";
 
 const MainSection = () => {
   return (
-    <section className="flex md:flex-row flex-col justify-around items-center md:p-20 w-full h-screen overflow-x-hidden">
-      <div className="flex flex-col justify-center gap-8 p-4 md:min-w-[360px] lg:min-w-[390px] text-3xl lg:text-4xl item-center">
+    <section className="flex md:flex-row flex-col justify-around items-center md:p-20 w-full h-screen overflow-hidden">
+      <div className="flex flex-col justify-center gap-4 lg:gap-8 p-4 md:min-w-[360px] text-2xl md:text-3xl lg:text-4xl item-center">
         <h1>
           Hi There!{" "}
           <span className="wave" role="img" aria-labelledby="wave">
@@ -18,11 +18,15 @@ const MainSection = () => {
           I'm
           <strong className="whitespace-nowrap"> Rajesh Ranjan !</strong>
         </h1>
-        <div className="font-semibold whitespace-nowrap">
-          <TypeText />
+        <div className="relative h-10 font-semibold whitespace-nowrap">
+          <div className="top-0 left-0 absolute">
+            <TypeText />
+          </div>
         </div>
+
         <SocialMedia />
       </div>
+
       <div className="flex justify-start items-start">
         <GreetingsImg />
       </div>
