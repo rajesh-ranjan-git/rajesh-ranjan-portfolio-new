9fa6249704192460cc9b030a5d9757d3af2ba21c
Rajesh Ranjan <rajesh.ranjan@aajtak.com>
2025-06-23 17:37:06 +0530

    dynamic favicon

--- src/components/title-manager/titleManager.tsx
+++ src/components/title-manager/titleManager.tsx
@@ -4,14 +4,29 @@ import { useEffect } from "react";
 
 const TitleManager = () => {
   const originalTitle = "Rajesh Ranjan | Full Stack Developer";
-  const inactiveTitle = "🙏 Come back soon! 👋";
+  const inactiveTitle = "Come back soon! 👋";
+
+  const originalFavicon = "/favicon-default.ico";
+  const awayFavicon = "/favicon-namaste.ico";
 
   useEffect(() => {
     const handleVisibilityChange = () => {
+      let favicon = document.querySelector("link[rel~='icon']") as HTMLLinkElement || null;
+      
+      if (!favicon) {
+        favicon = document.createElement('link');
+        favicon.rel = 'icon';
+        document.head.appendChild(favicon);
+      };
+
       if (document.hidden) {
         document.title = inactiveTitle;
+        console.log("debug favicon.href : ", favicon.href)
+        favicon.href = awayFavicon;
       } else {
         document.title = originalTitle;
+        console.log("debug favicon.href : ", favicon.href)
+        favicon.href = originalFavicon;
       }
     };
 
