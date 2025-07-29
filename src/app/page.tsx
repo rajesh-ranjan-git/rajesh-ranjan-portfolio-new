826383ff95dbfd509fbf359c1b25c45aaed75084
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-07-30 02:19:42 +0530

    social media

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import styles from "@/animations/greetings.module.css";
 import Sidebar from "@/components/sidebar/sidebar";
+import SocialMedia from "@/components/social-media/socialMedia";
 import GreetingsImg from "@/components/greetings-img/greetingsImg";
 
 export default function Home() {
@@ -7,21 +8,24 @@ export default function Home() {
     <>
       <Sidebar />
       <section className="flex flex-wrap justify-center items-center pl-24 w-screen min-h-screen max-h-screen overflow-hidden">
-        <div className="bg-red-400 h-full text-4xl leading-12">
-          <h1>
-            Hi There!{" "}
-            <span className={styles.wave} role="img" aria-labelledby="wave">
-              👋🏻
-            </span>
-          </h1>
+        <div className="flex flex-col justify-center">
+          <div className="h-full text-4xl leading-12">
+            <h1>
+              Hi There!{" "}
+              <span className={styles.wave} role="img" aria-labelledby="wave">
+                👋🏻
+              </span>
+            </h1>
 
-          <h1>
-            I'm
-            <strong>&nbsp;Rajesh Ranjan</strong>
-          </h1>
-          <div>
-            <p>Full Stack Developer</p>
+            <h1>
+              I'm
+              <strong>&nbsp;Rajesh Ranjan</strong>
+            </h1>
+            <div>
+              <p>Full Stack Developer</p>
+            </div>
           </div>
+          <SocialMedia />
         </div>
         <GreetingsImg />
       </section>
---
+++ src/components/social-media/socialMedia.tsx
@@ -0,0 +1,78 @@
+import Link from "next/link";
+import {
+  faGithubAlt,
+  faStackOverflow,
+  faLinkedinIn,
+  faInstagram,
+  faFacebook,
+  faGoogle,
+  faXTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const SocialMedia = () => {
+  return (
+    <div className="flex flex-wrap items-center gap-2 w-full">
+      <Link
+        href="https://github.com/rajesh-ranjan-git"
+        className="flex justify-center items-center bg-[#333] hover:bg-[#040b14] p-1 rounded-full w-12 h-12 transition-all ease-in-out"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faGithubAlt} size="2xl" color="#fff" />
+      </Link>
+
+      <Link
+        href="mailto:rajeshranjan8271@gmail.com"
+        className="flex justify-center items-center bg-[#ea4335] hover:bg-[#040b14] p-1 rounded-full w-12 h-12 transition-all ease-in-out"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faGoogle} size="2xl" color="#fff" />
+      </Link>
+
+      <Link
+        href="https://stackoverflow.com/users/7486676/rajesh-ranjan"
+        className="flex justify-center items-center bg-[#f48024] hover:bg-[#040b14] p-1 rounded-full w-12 h-12 transition-all ease-in-out"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faStackOverflow} size="2xl" color="#fff" />
+      </Link>
+      <Link
+        href="https://www.linkedin.com/in/rajesh-ranjan-660b1b13a"
+        className="flex justify-center items-center bg-[#0e76a8] hover:bg-[#040b14] p-1 rounded-full w-12 h-12 transition-all ease-in-out"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faLinkedinIn} size="2xl" color="#fff" />
+      </Link>
+      <Link
+        href="https://www.facebook.com/rajeshranjan8271/"
+        className="flex justify-center items-center bg-[#3b5998] hover:bg-[#040b14] p-1 rounded-full w-12 h-12 transition-all ease-in-out"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faFacebook} size="2xl" color="#fff" />
+      </Link>
+      <Link
+        href="https://x.com/_rajesh____"
+        className="flex justify-center items-center bg-[#000] hover:bg-[#040b14] p-1 rounded-full w-12 h-12 transition-all ease-in-out"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faXTwitter} size="2xl" color="#fff" />
+      </Link>
+      <Link
+        href="https://www.instagram.com/_rajesh____/"
+        className="flex justify-center items-center bg-[#e4405f] hover:bg-[#040b14] p-1 rounded-full w-12 h-12 transition-all ease-in-out"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faInstagram} size="2xl" color="#fff" />
+      </Link>
+    </div>
+  );
+};
+
+export default SocialMedia;
