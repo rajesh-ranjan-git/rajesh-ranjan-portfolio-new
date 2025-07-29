8b335b6d28517e00d04c786f719973ae31f4e3d6
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-07-30 00:09:50 +0530

    Starting over

--- src/store/store.tsx
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand";
-import { SidebarState } from "@/types/types";
-
-export const useSidebarStore = create<SidebarState>((set) => ({
-  sidebarToggle: false,
-  setSidebarToggle: (value) => set({ sidebarToggle: value }),
-}));
