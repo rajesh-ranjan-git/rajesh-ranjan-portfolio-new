40acdb01d4fb82fec652f4d621c627294881245a
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-07-30 02:56:39 +0530

    banner layout ready and responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import styles from "@/animations/greetings.module.css";
+import greetingsStyles from "@/animations/greetings.module.css";
+import buttonStyles from "@/animations/button.module.css";
 import Sidebar from "@/components/sidebar/sidebar";
 import SocialMedia from "@/components/social-media/socialMedia";
 import GreetingsImg from "@/components/greetings-img/greetingsImg";
@@ -8,11 +9,15 @@ export default function Home() {
     <>
       <Sidebar />
       <section className="flex flex-wrap justify-center items-center pl-24 w-screen min-h-screen max-h-screen overflow-hidden">
-        <div className="flex flex-col justify-center gap-2">
+        <div className="flex flex-col justify-center gap-6 bg-red-300 pt-10">
           <div className="h-full text-4xl leading-12">
             <h1>
               Hi There!{" "}
-              <span className={styles.wave} role="img" aria-labelledby="wave">
+              <span
+                className={greetingsStyles.wave}
+                role="img"
+                aria-labelledby="wave"
+              >
                 👋🏻
               </span>
             </h1>
@@ -26,8 +31,33 @@ export default function Home() {
             </div>
           </div>
           <SocialMedia />
+          <div className="flex items-center gap-2">
+            <div className="relative bg-white hover:shadow-md rounded-md overflow-hidden font-semibold text-xl active:scale-95 duration-500">
+              <span className="absolute flex justify-center items-center m-auto rounded-md w-full h-full overflow-hidden font-semibold text-[#3f82d9] text-xl text-center">
+                Contact Me
+              </span>
+              <button
+                className={`bg-[#3f82d9] p-4 border-none w-full h-full text-white cursor-pointer mask-no-repeat ${buttonStyles.btn_mask_1}`}
+              >
+                Contact Me
+              </button>
+            </div>
+
+            <div className="relative bg-white hover:shadow-md rounded-md overflow-hidden font-semibold text-xl active:scale-95 duration-500">
+              <span className="absolute flex justify-center items-center m-auto rounded-md w-full h-full overflow-hidden font-semibold text-[#000] text-xl text-center">
+                See My Resume
+              </span>
+              <button
+                className={`bg-[#000] p-4 border-none w-full h-full text-white cursor-pointer mask-no-repeat ${buttonStyles.btn_mask_2}`}
+              >
+                See My Resume
+              </button>
+            </div>
+          </div>
         </div>
-        <GreetingsImg />
+        <div className="bg-blue-300">
+          <GreetingsImg />
+        </div>
       </section>
     </>
   );
