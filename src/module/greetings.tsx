501b99f9224dab0a28a0808e45387bcef6e0850d
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-07-30 23:18:23 +0530

    rectangle

--- src/module/greetings.tsx
+++ src/module/greetings.tsx
@@ -13,6 +13,12 @@ const Greetings = () => {
       <div
         className={`w-[250vh] h-1/3 absolute left-0 opacity-0 ${greetingsStyles.rectangle} ${greetingsStyles.rectangle_2}`}
       ></div>
+      <div
+        className={`border-16 border-white opacity-20 absolute lg:w-[40rem] lg:h-[40rem] md:w-[30rem] md:h-[30rem] w-[20rem] h-[20rem] block lg:left-[-20%] lg:bottom-[-30%] left-[-5%] bottom-[-10%] ${greetingsStyles.rectangle_transparent}`}
+      ></div>
+      <div
+        className={`border-16 border-white opacity-20 absolute block lg:w-[45rem] lg:h-[45rem] md:w-[35rem] md:h-[35rem] w-[25rem] h-[25rem] right-[-10%] top-[5%] ${greetingsStyles.rectangle_transparent}`}
+      ></div>
       <GreetingsMain />
       <GreetingsImg />
     </section>
