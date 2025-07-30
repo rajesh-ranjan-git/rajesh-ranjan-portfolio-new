5d5305c14e2c63f4b7303d7f7302d682af92adca
Rajesh Ranjan <rajesh.ranjan@aajtak.com>
2025-07-31 03:35:06 +0530

    snap scroll and scrollbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,11 @@ import PersonalInfo from "@/module/personalInfo";
 
 export default function Home() {
   return (
-    <>
+    <main className="h-screen overflow-x-hidden overflow-y-scroll snap-mandatory snap-y">
       <Sidebar />
       <Greetings />
       <About />
       <PersonalInfo />
-    </>
+    </main>
   );
 }
--- src/module/personalInfo.tsx
+++ src/module/personalInfo.tsx
@@ -4,7 +4,7 @@ import ProfileImgCircular from "@/components/profile-img/profileImgCircular";
 const PersonalInfo = () => {
   return (
     <section
-      className={`before:block before:top-0 before:right-0 before:bottom-0 before:left-0 before:absolute relative before:inset-0 flex flex-wrap justify-around items-center before:opacity-100 pr-2 md:pr-10 pl-20 md:pl-28 w-screen min-h-screen max-h-screen overflow-hidden greetings`}
+      className={`before:block before:top-0 before:right-0 before:bottom-0 before:left-0 before:absolute relative before:inset-0 flex flex-wrap justify-around items-center before:opacity-100 pr-2 md:pr-10 pl-20 md:pl-28 w-screen min-h-screen max-h-screen overflow-hidden snap-start`}
     >
       <PersonalInfoText />
       <ProfileImgCircular />
