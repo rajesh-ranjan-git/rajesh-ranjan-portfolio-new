eac7f30d62f6630ac966a470dfd67cb6c30886c7
Rajesh Ranjan <rajesh.ranjan@aajtak.com>
2025-07-30 13:25:28 +0530

    decided with image not svg

--- src/module/greetings.tsx
+++ src/module/greetings.tsx
@@ -1,22 +1,11 @@
-import Image from "next/image";
-import bannerImage from "@/assets/personal-img/profile_photo_3d_cartoon.png";
-import GreetingsImg from "@/components/greetings/greetings-img/greetingsImg";
 import GreetingsMain from "@/containers/greetings/greetingsMain";
+import GreetingsImg from "@/components/greetings/greetings-img/greetingsImg";
 
 const Greetings = () => {
   return (
-    <section className="flex flex-wrap justify-center items-center pl-24 w-screen min-h-screen max-h-screen overflow-hidden">
+    <section className="flex flex-wrap justify-around items-center pl-24 w-screen min-h-screen max-h-screen overflow-hidden">
       <GreetingsMain />
-      {/* <GreetingsImg /> */}
-      <div className="flex justify-center items-center bg-red-400">
-        <Image
-          src={bannerImage}
-          alt="profileImg"
-          width={200}
-          height={200}
-          className="rounded-2xl w-full h-full object-cover"
-        />
-      </div>
+      <GreetingsImg />
     </section>
   );
 };
