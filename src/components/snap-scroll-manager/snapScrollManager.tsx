58e67fba0420b127fc550eb74c1eebc754947dbd
Rajesh Ranjan <rajesh.ranjan@aajtak.com>
2025-07-31 13:17:08 +0530

    added scroll to top with smooth effect on route change

--- src/components/snap-scroll-manager/snapScrollManager.tsx
+++ src/components/snap-scroll-manager/snapScrollManager.tsx
@@ -1,9 +1,21 @@
 "use client";
 
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 import useSnapScroll from "@/hooks/useSnapScroll";
 
 export default function SnapScrollManager() {
   // Hook for managing smooth scroll
   useSnapScroll(500);
+
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const main = document.querySelector("main");
+    if (main) {
+      main.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  }, [pathname]);
+
   return null;
 }
