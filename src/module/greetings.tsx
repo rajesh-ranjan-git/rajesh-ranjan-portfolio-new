afe10d4577c43386799e5f4749ea695b3684b434
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-08-03 13:05:52 +0530

    removed container

--- src/module/greetings.tsx
+++ src/module/greetings.tsx
@@ -1,5 +1,7 @@
 import greetingsStyles from "@/animations/greetings.module.css";
-import GreetingsMain from "@/containers/greetings/greetingsMain";
+import { ButtonMask1, ButtonMask2 } from "@/components/buttons/buttonMask";
+import GreetingsText from "@/components/greetings/greetings-text/greetingsText";
+import SocialMedia from "@/components/social-media/socialMedia";
 import GreetingsImg from "@/components/greetings/greetings-img/greetingsImg";
 import GreetingsBg from "@/components/greetings/greetings-bg/greetingsBg";
 
@@ -9,7 +11,17 @@ const Greetings = () => {
       className={`before:block before:top-0 before:right-0 before:bottom-0 before:left-0 before:absolute relative before:inset-0 flex flex-wrap justify-around items-center before:opacity-100 pr-2 md:pr-10 pl-20 md:pl-28 w-screen min-h-screen max-h-screen overflow-hidden ${greetingsStyles.greetings}`}
     >
       <GreetingsBg />
-      <GreetingsMain />
+      <div className="flex flex-col flex-grow justify-center gap-6 pl-0 md:pl-10 w-full lg:w-[60%] h-full">
+        <GreetingsText />
+        <SocialMedia />
+        <div className="flex items-center gap-2 md:gap-6 w-full">
+          <ButtonMask1 btnText={"Contact Me"} className="text-sm md:text-lg" />
+          <ButtonMask2
+            btnText={"See My Resume"}
+            className="text-sm md:text-lg"
+          />
+        </div>
+      </div>
       <GreetingsImg />
     </section>
   );
