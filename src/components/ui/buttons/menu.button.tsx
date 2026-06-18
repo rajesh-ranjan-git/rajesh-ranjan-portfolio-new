50aa0c8d7288a9d22ef7ab0d85e2c6d1afcc8451
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2026-06-18 22:05:08 +0530

    menu active bg

--- src/components/ui/buttons/menu.button.tsx
+++ src/components/ui/buttons/menu.button.tsx
@@ -11,7 +11,7 @@ const MenuButton = () => {
     <button
       type="button"
       data-sidebar-menu-button="true"
-      className="bg-[#040B14] bg-none p-2 border-0 rounded-md outline-none font-bold text-2xl transition-all ease-in-out hover:bg-accent-blue cursor-pointer"
+      className={`p-2 border-0 rounded-md outline-none font-bold text-2xl transition-all ease-in-out hover:bg-accent-blue cursor-pointer ${sidebarToggle ? "bg-accent-blue" : "bg-[#040B14]"}`}
       onMouseDown={(event) => event.stopPropagation()}
       onClick={(event) => {
         event.stopPropagation();
