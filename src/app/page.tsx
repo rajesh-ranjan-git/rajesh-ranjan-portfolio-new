47ef892327971e30d0b18d15e7a8d25fc8aaf79d
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2026-06-19 17:15:59 +0530

    working on about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Greetings from "@/sections/greetings";
+import About from "@/sections/about";
 
 export default function Home() {
   return (
-    <main className="h-screen overflow-x-hidden">
+    <main className="min-h-screen overflow-x-hidden">
       <Greetings />
+      <About />
     </main>
   );
 }
---
+++ src/sections/about.tsx
@@ -0,0 +1,35 @@
+import aboutStyles from "@/animations/about.module.css";
+import Image from "next/image";
+
+const About = () => {
+  return (
+    <div className="flex justify-around items-center bg-[#0d0f2b] min-h-screen">
+      <div className="isolate relative flex justify-center items-center bg-[#1a1f4e] p-4 rounded-[84px]">
+        <div className="top-0 left-0 -z-1 absolute inset-0 bg-white/20 rounded-[84px] -rotate-6"></div>
+        <div className="top-0 left-0 -z-1 absolute inset-0 bg-white/30 rounded-[84px] -rotate-12"></div>
+
+        <div
+          className={`top-20 -right-12 absolute bg-white px-6 py-4 rounded-[36px] font-arima font-bold text-2xl duration-500 ${aboutStyles.float}`}
+        >
+          7+ yrs
+        </div>
+        <div
+          className={`bottom-20 -left-12 absolute bg-white px-6 py-4 rounded-[36px] font-arima font-bold text-2xl duration-500 ${aboutStyles.float_reverse}`}
+        >
+          Full Stack Developer
+        </div>
+
+        <Image
+          src="/assets/personal-img/profile_photo_3d_cartoon.png"
+          alt="Image"
+          width="300"
+          height="300"
+          className="rounded-br-[160px]"
+        />
+      </div>
+      <div> About Rajesh Ranjan</div>
+    </div>
+  );
+};
+
+export default About;
