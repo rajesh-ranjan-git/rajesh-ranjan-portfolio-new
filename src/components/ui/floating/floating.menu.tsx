e7bf447c99347ad9420073df637653cd9eb8b55a
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2026-06-22 16:55:17 +0530

    refactor

--- src/components/ui/floating/floating.menu.tsx
+++ src/components/ui/floating/floating.menu.tsx
@@ -1,9 +1,9 @@
-import ThemeToggle from "../../theme/theme.toggle";
-import MenuButton from "../buttons/menu.button";
+import ThemeToggle from "@/components/theme/theme.toggle";
+import MenuButton from "@/components/ui/buttons/menu.button";
 
 const FloatingMenu = () => {
   return (
-    <div className="top-4 right-4 z-100 fixed flex items-center gap-2 py-2">
+    <div className="top-0 md:top-2 right-2 z-100 fixed flex items-center gap-2 py-2">
       <ThemeToggle />
 
       <div className="md:hidden">
