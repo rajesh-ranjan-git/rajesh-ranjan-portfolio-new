f3022822f2c19f63b689c6e3eba5c29c253a0b95
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2026-07-18 09:05:04 +0530

    remove motion from form error

--- src/components/ui/forms/form.error.tsx
+++ src/components/ui/forms/form.error.tsx
@@ -1,21 +1,14 @@
-import { motion, AnimatePresence } from "motion/react";
 import { FormErrorMessageProps } from "@/types/props/forms.props.types";
 
 const FormErrorMessage = ({ error, className }: FormErrorMessageProps) => {
   return (
-    <AnimatePresence>
-      {error && (
-        <motion.p
-          initial={{ opacity: 0, height: 0 }}
-          animate={{ opacity: 1, height: "auto" }}
-          exit={{ opacity: 0, height: 0 }}
-          transition={{ duration: 0.25, ease: "easeOut" }}
-          className={`ml-2 my-0.5 overflow-hidden text-error text-xs origin-top ${className}`}
-        >
-          {error}
-        </motion.p>
-      )}
-    </AnimatePresence>
+    <p
+      className={`ml-2 my-0.5 overflow-hidden text-error text-xs origin-top transition-all duration-500 ${
+        error ? "max-h-10 opacity-100" : "max-h-0 opacity-0"
+      } ${className}`}
+    >
+      {error}
+    </p>
   );
 };
 
