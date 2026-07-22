79c7e2ec2ca651cccf8bab1269215d3ec54929d1
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2026-07-22 10:47:33 +0530

    splash done

--- src/components/ui/splash/splash.loader.tsx
+++ src/components/ui/splash/splash.loader.tsx
@@ -1,12 +1,35 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import splashStyles from "@/animations/splash.module.css";
 
 export interface SplashLoaderProps {
   loadingText?: string;
 }
 
+interface FazerStyle {
+  "--top": string;
+  "--width": string;
+  "--duration": string;
+  "--delay": string;
+}
+
 export default function SplashLoader({
   loadingText = "Loading...",
 }: SplashLoaderProps) {
+  const [fazers, setFazers] = useState<FazerStyle[]>([]);
+
+  useEffect(() => {
+    setFazers(
+      Array.from({ length: 20 }, () => ({
+        "--top": `${Math.random() * 100}%`,
+        "--width": `${12 + Math.random() * 20}%`,
+        "--duration": `${0.5 + Math.random() * 0.7}s`,
+        "--delay": `${-Math.random() * 8}s`,
+      })),
+    );
+  }, []);
+
   return (
     <div className="top-1/2 left-1/2 absolute w-full h-full -translate-x-1/2 -translate-y-1/2">
       <div
@@ -27,18 +50,8 @@ export default function SplashLoader({
       </div>
 
       <div className={splashStyles.splash_longfazers}>
-        {Array.from({ length: 20 }).map((_, i) => (
-          <span
-            key={i}
-            style={
-              {
-                "--top": `${Math.random() * 100}%`,
-                "--width": `${12 + Math.random() * 20}%`,
-                "--duration": `${0.5 + Math.random() * 0.7}s`,
-                "--delay": `${-Math.random() * 8}s`,
-              } as React.CSSProperties
-            }
-          />
+        {fazers.map((style, i) => (
+          <span key={i} style={style as React.CSSProperties} />
         ))}
       </div>
 
