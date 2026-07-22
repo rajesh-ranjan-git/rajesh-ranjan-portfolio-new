972f18f0cea20ccb3c2f197b2f502a28a38dc261
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2026-07-22 12:52:12 +0530

    splash tailwind refactor

--- src/components/ui/splash/splash.loader.tsx
+++ src/components/ui/splash/splash.loader.tsx
@@ -22,38 +22,48 @@ export default function SplashLoader({
   }, []);
 
   return (
-    <div className="top-1/2 left-1/2 absolute w-full h-full -translate-x-1/2 -translate-y-1/2">
+    <div className="w-full h-full">
       <div
         className={`absolute w-full h-full inset-0 ${splashStyles.splash_fly}`}
       >
-        <div className={`${splashStyles.splash_body}`}>
+        <div
+          className={`absolute top-1/2 left-1/2 ${splashStyles.splash_body}`}
+        >
           <span className={`${splashStyles.splash_wing}`}>
-            <span />
-            <span />
-            <span />
-            <span />
+            <span className="absolute bg-fixed-light w-7.5 h-px" />
+            <span className="absolute bg-fixed-light w-7.5 h-px" />
+            <span className="absolute bg-fixed-light w-7.5 h-px" />
+            <span className="absolute bg-fixed-light w-7.5 h-px" />
           </span>
-          <div className={splashStyles.splash_base}>
-            <span />
-            <div className={splashStyles.splash_face} />
+          <div className={`absolute w-0 h-0 ${splashStyles.splash_base}`}>
+            <span className="absolute before:absolute after:absolute before:bg-fixed-light before:rounded-full w-0 before:w-6 after:w-0 h-0 before:h-6 after:h-0" />
+            <div
+              className={`absolute w-5 h-3 bg-fixed-light after:absolute after:w-3 after:h-3 after:bg-fixed-light ${splashStyles.splash_face}`}
+            />
           </div>
         </div>
       </div>
 
       <div className={splashStyles.splash_longfazers}>
         {fazers.map((style, i) => (
-          <span key={i} style={style as React.CSSProperties} />
+          <span
+            key={i}
+            className="absolute bg-fixed-light/60"
+            style={style as React.CSSProperties}
+          />
         ))}
       </div>
 
       <h1
-        className={`bottom-[30%] left-1/2 absolute font-semibold text-white whitespace-nowrap -translate-x-1/2 font-alkatra ${splashStyles.splash_loader}`}
+        className={`bottom-[30%] left-1/2 absolute font-semibold text-white whitespace-nowrap -translate-x-1/2 font-alkatra inline-block ${splashStyles.splash_loader}`}
       >
         {loadingText}
       </h1>
 
       <div className="bottom-[20%] left-1/2 absolute w-1/2 md:w-1/4 -translate-x-1/2">
-        <span className={splashStyles.splash_progress}></span>
+        <span
+          className={`inline-block relative w-0 after:absolute before:absolute before:opacity-0 after:opacity-0 ${splashStyles.splash_progress}`}
+        ></span>
       </div>
     </div>
   );
